b9a625802c5e822ca32d03e1fa564635127291a6
孙翛然 <c.salt@hotmail.com>
2018-05-06 16:58:37 +0800

    Use FileStorage in Vue instances

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Flux from './plugins/flux'
 import I18N from './plugins/i18n'
+import FileStorage from './plugins/storage'
 import Root from './components/root'
 import Store from './components/store'
 
 Vue.use(I18N)
+Vue.use(FileStorage)
 Vue.use(Flux, Store)
 
 new Vue(Root)
