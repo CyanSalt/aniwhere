854262d73a056cd4e9d76f548652e28bd6029602
孙翛然 <c.salt@hotmail.com>
2018-05-07 00:31:15 +0800

    Support program searching

--- src/components/suggestion.vue
+++ src/components/suggestion.vue
@@ -9,6 +9,8 @@
 <script>
 import debounce from 'lodash.debounce'
 import {ipcRenderer, remote} from 'electron'
+import {join, basename} from 'path'
+import {readdir, lstat} from 'original-fs'
 import SuggestionItem from './suggestion-item'
 import {state} from '../plugins/flux'
 
@@ -19,7 +21,14 @@ export default {
   data() {
     return {
       suggestions: [],
+      searchedAt: 0,
       selected: -1,
+      cache: {
+        programs: {
+          list: [],
+          cachedAt: 0,
+        },
+      }
     }
   },
   computed: {
@@ -60,6 +69,7 @@ export default {
       })
     },
     search(value) {
+      this.searchedAt = Date.now()
       if (!value) {
         this.suggestions = []
         return
@@ -70,8 +80,67 @@ export default {
         this.querySearchEngines(value),
       )
     },
+    // Search Programs
     queryPrograms(value) {
+      const paths = this.settings['suggestions.programPaths'] || []
+      const exts = this.settings['suggestions.programExts'] || []
+      const duration = this.settings['suggestions.caching'] || 60
+      const cache = this.cache.programs
+      if (this.searchedAt - cache.cachedAt < duration * 1000) {
+        return cache.list
+          .filter(program => this.matchProgram(program, value))
+          .map(this.getProgramEntry)
+      }
+      cache.cachedAt = this.searchedAt
+      for (const path of paths) {
+        const realpath = path.replace(/%([^%]+)%/g, (full, name) => {
+          return process.env[name]
+        })
+        this.searchProgramsIn(value, realpath, exts, this.searchedAt)
+      }
       return []
+    },
+    // eslint-disable-next-line max-params
+    searchProgramsIn(value, path, exts, start) {
+      readdir(path, (readdirerr, files) => {
+        if (readdirerr) return
+        for (const file of files) {
+          const fullpath = join(path, file)
+          lstat(fullpath, (lstaterr, stats) => {
+            if (lstaterr) return
+            if (stats.isDirectory()) {
+              this.searchProgramsIn(value, fullpath, exts, start)
+              return
+            }
+            const ext = exts.find(extname => {
+              return file.slice(-extname.length) === extname
+            })
+            if (ext) {
+              const name = basename(file, ext)
+              const program = {name, path: fullpath}
+              if (start === this.cache.programs.cachedAt) {
+                this.cache.programs.list.push(program)
+              }
+              if (start === this.searchedAt &&
+                this.matchProgram(program, value)
+              ) {
+                this.suggestions.unshift(this.getProgramEntry(program))
+              }
+            }
+          })
+        }
+      })
+    },
+    matchProgram(program, value) {
+      return program.name.indexOf(value) !== -1
+    },
+    getProgramEntry(program) {
+      return {
+        type: 'program',
+        category: 'program',
+        url: '',
+        text: program.name
+      }
     },
     queryDocuments(value) {
       return []
