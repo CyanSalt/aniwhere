21eca238497ac37b120ee5a6cae6a9c86a8eebda
孙翛然 <c.salt@hotmail.com>
2018-05-31 11:44:38 +0800

    Add drag handler to the right of typewriter

--- src/components/typewriter.vue
+++ src/components/typewriter.vue
@@ -1,6 +1,9 @@
 <template>
-  <input class="typewriter" type="text" v-model.trim="input" @focus="focus"
-    :placeholder="i18n('Search Anything#!1')" autofocus>
+  <div class="typewriter">
+    <input class="input" type="text" v-model.trim="input" @focus="focus"
+      :placeholder="i18n('Search Anything#!1')" autofocus>
+    <div class="drag-handler"></div>
+  </div>
 </template>
 
 <script>
@@ -31,20 +34,29 @@ export default {
 
 <style>
 .typewriter {
+  width: 100%;
+  display: flex;
+}
+.typewriter .input {
   -webkit-appearance: none;
   box-sizing: border-box;
   padding: 0 16px;
   height: 54px;
   line-height: 54px;
   font-size: 24px;
-  width: 100%;
+  flex-grow: 1;
   border: none;
   outline: none;
   font-family: inherit;
   background: transparent;
 }
-.typewriter::placeholder {
+.typewriter .input::placeholder {
   color: rgba(0, 0, 0, 0.2);
   font-style: italic;
+}
+.typewriter .drag-handler {
+  width: 20px;
+  -webkit-app-region: drag;
+  user-select: none;
 }
 </style>
