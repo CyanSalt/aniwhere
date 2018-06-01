c28cf327ac8aa34b7f98c39a802982a883717ae9
孙翛然 <c.salt@hotmail.com>
2018-06-01 14:26:50 +0800

    Add demo of customization files

---
+++ src/resources/default/custom.js
@@ -0,0 +1,22 @@
+/**
+ * Your init script
+ * @param {Vue} vm ViewModal for root element of Vue.js
+ * @return {void}
+ */
+module.exports = function (vm) {
+
+  // an example to hack your software
+  // vm.$flux.emit('suggestions/register', value => {
+  //   const result = []
+  //   if (value === 'time') {
+  //     const now = new Date.toString()
+  //     result.push({
+  //       type: 'clipboard',
+  //       link: now,
+  //       title: `= <strong>${now}</strong>`,
+  //     })
+  //   }
+  //   return result
+  // })
+
+}
